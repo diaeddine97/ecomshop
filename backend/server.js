873b0921ca77d75fbb6306a42a7d2e0ff72416b7
diaeddine <diaeddine97@gmail.com>
2020-12-31 00:20:38 +0100

    models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
-dotenv.config()
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -15,9 +19,15 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p=> p._id === req.params.id)
+  const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port : ${PORT}`));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port : ${PORT}`.yellow
+      .bold
+  )
+);
