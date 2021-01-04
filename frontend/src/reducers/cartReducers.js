3ac10234639334bbfc5489189f8b12561853597d
diaeddine <diaeddine97@gmail.com>
2021-01-04 17:09:35 +0100

    cart action, reducer part 2, card page

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from '../constants/cardConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cardConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -10,6 +10,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           return { ...state, cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x)}
       } else {
         return { ...state, cartItems: [...state.cartItems, item] };
+      }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(x=> x.product !== action.payload)
       }
     default:
       return state;
