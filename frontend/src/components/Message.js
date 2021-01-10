4308cc995998c86f903bbfffd27be09ad3ab2cf8
diaeddine <diaeddine97@gmail.com>
2021-01-10 17:54:37 +0100

    separating route and controller of product

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,15 +1,11 @@
-import React from 'react'
-import { Alert } from 'react-bootstrap'
+import React from 'react';
+import { Alert } from 'react-bootstrap';
 
-const Message = ({variant, children}) => {
-    return (
-        <Alert variant={variant}>
-            {children}
-        </Alert>
-    )
-}
+const Message = ({ variant, children }) => {
+  return <Alert variant={variant}>{children}</Alert>;
+};
 
-Message.defaultPros = {
-    variant: 'info',
-}
-export default Message
+Message.defaultProps = {
+  variant: 'info',
+};
+export default Message;
